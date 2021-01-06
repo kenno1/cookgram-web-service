9a8e7d21d47a9843222c767d6bf7fb0c2e8f8f4e
kambe.kenta <kenta.k199405@gmail.com>
2021-01-06 21:42:19 +0900

    added user's list button to header.

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6a951946026ddbbf7a4e365c1d9497da5269d4dec775c93094a361387aec342392eadc1373b5ee998980991d6e204216b090ba7d3eef9fbcf8a3867d032b7484'
