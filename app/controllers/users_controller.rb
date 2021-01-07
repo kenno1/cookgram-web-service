c841ed1f1eb5a6f17fc0ba321f8daf0af82d5a22
kambe.kenta <kenta.k199405@gmail.com>
2021-01-07 22:20:31 +0900

    added flush message function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), notice: 'Success to create!'
     else
       render :edit
     end
