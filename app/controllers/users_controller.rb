c675745fdc87cdc1997cf2d27fbe47ce4313bd91
kambe.kenta <kenta.k199405@gmail.com>
2021-01-07 22:04:29 +0900

    added access validation for user and recipe.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: 'Invalid Access.'
+    end
   end
 
   def update
